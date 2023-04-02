5a524cdfc5f964eb4c5565eb14fa150537c23132
UniqueflowSA <saco2020a@gmail.com>
2023-04-03 00:53:59 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -3,8 +3,17 @@ function solution(s1, s2) {
     s1.forEach((item)=>{
         s2.forEach((item2)=>{
             item === item2 ? answer++:answer
-            
         })
     })
     return answer;
 }
+
+// function solution(s1, s2) {
+//   let answer = 0;
+//   s1.forEach((item) => {
+//     if (s2.includes(item)) {
+//       answer++;
+//     }
+//   });
+//   return answer;
+// }
